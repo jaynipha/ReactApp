5478207d72c0a5be60f35cf3f8e1351771c67888
jaynipha <josephjenny490@gmail.com>
2023-01-26 15:36:55 +0100

    node modules

--- project.js
+++ project.js
@@ -143,3 +143,37 @@ const navbar = (
 );
 console.log(navbar);
 ReactDOM.render(navbar, document.getElementById('root'));
+
+const page = (
+	<div>
+		<h1>My awesome website in React</h1>
+		<h3>Reasons I love React</h3>
+		<ol>
+			<li>It's composable</li>
+			<li>It's declarative</li>
+			<li>It's a hireable skill</li>
+			<li>It's actively maintained by skilled people</li>
+		</ol>
+	</div>
+);
+
+ReactDOM.render(page, document.getElementById('root'));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const pages = (
+	<div>
+		<img src="./react-logo.png" width="40px" />
+		<h1>Fun facts about React</h1>
+		<ul>
+			<li>Was first released in 2013</li>
+			<li>Was originally created by Jordan Walke</li>
+			<li>Has well over 100K stars on GitHub</li>
+			<li>Is maintained by Facebook</li>
+			<li>Powers thousands of enterprise apps, including mobile apps</li>
+		</ul>
+	</div>
+);
+
+ReactDOM.render(pages, document.getElementById('root'));
